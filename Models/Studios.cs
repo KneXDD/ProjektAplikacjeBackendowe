9cdddb2929f4eb05e96bf1768d6f647205b2a39d
KneX <20730247+KneXDD@users.noreply.github.com>
2022-04-13 18:05:31 +0200

    Add Enum Countries.cs Add Model Studios.cs

---
+++ Models/Studios.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GameHelperApp.Enums;
+
+namespace GameHelperApp.Models;
+
+public class Studios
+{
+    [Key]
+    public int StudioId { get; set; }
+
+    public String Name { get; set; }
+
+    public DateTime YearOfEstablishment { get; set; }
+
+    public Countries Country { get; set; }
+
+    [Column(TypeName = "nvarchar(20000)")]
+    public String CompanyDescriptiopn { get; set; }
+
+    public Byte[] Logo { get; set; }
+
+    public int NumberOfWorkers { get; set; }
+}
